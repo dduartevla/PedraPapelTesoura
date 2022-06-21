c02142b5ce6dbfd5712afce143bd014f17a5aa1a
Débora Izabel Rocha Duarte <deboraduarte@ice.ufjf.br>
2022-06-21 13:59:37 -0300

    Capturar os compenentes de entrada e saída e criação dos métodos vazios dos botões Pedra, Papel e Tesoura. Vinculação dos métodos com os botões.

--- app/src/main/java/ufjf/dcc/debora/pedrapapeltesoura/MainActivity.java
+++ app/src/main/java/ufjf/dcc/debora/pedrapapeltesoura/MainActivity.java
@@ -3,12 +3,50 @@ package ufjf.dcc.debora.pedrapapeltesoura;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button buttonPedra;
+    private Button buttonPapel;
+    private Button buttonTesoura;
+
+    private ProgressBar progressBarComputador;
+    private ProgressBar progressBarHumano;
+
+    private TextView textViewStatus;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Componentes de entrada
+        buttonPapel = findViewById(R.id.buttonPapel);
+        buttonPedra = findViewById(R.id.buttonPedra);
+        buttonTesoura = findViewById(R.id.buttonTesoura);
+
+        //Componentes de Saída
+        progressBarComputador = findViewById(R.id.progressBarComputador);
+        progressBarHumano = findViewById(R.id.progressBarHumano);
+
+        textViewStatus = findViewById(R.id.textViewStatus);
+
+    }
+
+    public void buttonPedraClick(View view){
+
+    }
+
+    public void buttonPapelClick(View view){
+
+    }
+
+    public void buttonTesouraClick(View view){
+
     }
 }
